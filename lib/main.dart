3a5c28b0d035c5f0b3a0f3d782e9a6f6355433fb
bansalmohit123 <149596867+bansalmohit123@users.noreply.github.com>
2023-11-02 00:57:11 +0530

    Made minor changes to bank interface

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
       theme: theme,
       title: 'tanisha_s_application14',
       debugShowCheckedModeBanner: false,
-      initialRoute: AppRoutes.profileScreen,
+      initialRoute: AppRoutes.bankToBankTransferAmountIcicScreen,
       routes: AppRoutes.routes,
     );
   }
