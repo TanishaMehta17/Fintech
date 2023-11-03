0ec75cb7805acbae45663ccd456e93d613b28ac4
nikhilmishra1710 <nikhilmishra1710@gmail.com>
2023-11-04 01:56:04 +0530

    Update eid_password_for_transfare_conformation_screen.dart

--- lib/presentation/eid_password_for_transfare_conformation_screen/eid_password_for_transfare_conformation_screen.dart
+++ lib/presentation/eid_password_for_transfare_conformation_screen/eid_password_for_transfare_conformation_screen.dart
@@ -8,6 +8,11 @@ import 'package:tanisha_s_application14/widgets/app_bar/appbar_subtitle_2.dart';
 import 'package:tanisha_s_application14/widgets/app_bar/custom_app_bar.dart';
 import 'package:tanisha_s_application14/widgets/custom_bottom_bar.dart';
 import 'package:tanisha_s_application14/widgets/custom_elevated_button.dart';
+import 'package:tanisha_s_application14/widgets/custom_pin_code_text_field.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:provider/provider.dart';
+import '../customer_id_provider.dart';
 
 // ignore_for_file: must_be_immutable
 class EidPasswordForTransfareConformationScreen extends StatelessWidget {
@@ -19,7 +24,8 @@ class EidPasswordForTransfareConformationScreen extends StatelessWidget {
   final FocusNode second = FocusNode();
   final FocusNode third = FocusNode();
   final FocusNode fourth = FocusNode();
-  void onTapVerifyotp(BuildContext context) {
+
+  Future<void> onTapConfirm(BuildContext context, dynamic cid) async {
     if (number_controller.text.isEmpty ||
         number_controller1.text.isEmpty ||
         number_controller2.text.isEmpty ||
@@ -35,8 +41,77 @@ class EidPasswordForTransfareConformationScreen extends StatelessWidget {
       );
     } else {
       // All OTP fields are filled, navigate to the next screen.
-      Navigator.pushNamed(
-          context, AppRoutes.eidGiftConfirmationSuccessfulTransferScreen);
+      // Navigator.pushNamed(context, AppRoutes.homeLoanPaymentDoneScreen);
+      String dig1 = number_controller.text;
+      String dig2 = number_controller1.text;
+      String dig3 = number_controller2.text;
+      String dig4 = number_controller3.text;
+      String pin = dig1 + dig2 + dig3 + dig4;
+
+      if (cid != null) {
+        var response = await http.post(
+          Uri.parse("http://localhost:5000/payment_interface"),
+          headers: {
+            "Content-Type": "application/json",
+            "token": cid,
+            "pin": pin
+          },
+          body: jsonEncode({"event": "gift_card_payment", "scheme": "eid"}),
+        );
+
+        if (response.statusCode == 200) {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) =>
+                    EidGiftConfirmationSuccessfulTransferScreen(
+                        name, accNo, amt)),
+          );
+        } else if (response.statusCode == 400) {
+          ///Invalid PIN
+          showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                title: Text('Error'),
+                content: Text('Invalid credentials. Please try again.'),
+                actions: <Widget>[
+                  TextButton(
+                    child: Text('OK'),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ],
+              );
+            },
+          );
+        } else if (response.statusCode == 401) {
+          ///Login screen redirect session expire
+          Navigator.pushNamed(context, AppRoutes.loginScreen);
+        } else {
+          ///Some error occured
+          showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                title: Text('Error'),
+                content: Text('Some error occured. Please try again later.'),
+                actions: <Widget>[
+                  TextButton(
+                    child: Text('OK'),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ],
+              );
+            },
+          );
+        }
+      } else {
+        Navigator.pushNamed(context, AppRoutes.loginScreen);
+      }
     }
   }
 
@@ -49,6 +124,12 @@ class EidPasswordForTransfareConformationScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var customerId = Provider.of<CustomerIdProvider>(context).customerId;
+    if (customerId != null)
+      customerId = customerId;
+    else
+      customerId = null;
+
     mediaQueryData = MediaQuery.of(context);
     return SafeArea(
         child: Scaffold(
@@ -336,7 +417,7 @@ class EidPasswordForTransfareConformationScreen extends StatelessWidget {
                                           text: 'Confirm',
                                           width: 350,
                                           onTap: () {
-                                            onTapVerifyotp(context);
+                                            onTapConfirm(context, customerId);
                                           },
                                         ),
                                       )
@@ -366,12 +447,4 @@ class EidPasswordForTransfareConformationScreen extends StatelessWidget {
   //   Navigator.pushNamed(
   //       context, AppRoutes.eidGiftConfirmationSuccessfulTransferScreen);
   // }
-  onTapConfirm(BuildContext context) {
-    Navigator.push(
-      context,
-      MaterialPageRoute(
-          builder: (context) =>
-              EidGiftConfirmationSuccessfulTransferScreen(name, accNo, amt)),
-    );
-  }
 }
