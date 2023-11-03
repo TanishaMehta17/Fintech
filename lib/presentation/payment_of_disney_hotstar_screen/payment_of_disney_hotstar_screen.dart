174a1bf17f39fe28ca5bdc75d197a52df680be07
nikhilmishra1710 <nikhilmishra1710@gmail.com>
2023-11-03 23:55:45 +0530

    Updated Disney+hotstar query part

--- lib/presentation/payment_of_disney_hotstar_screen/payment_of_disney_hotstar_screen.dart
+++ lib/presentation/payment_of_disney_hotstar_screen/payment_of_disney_hotstar_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:tanisha_s_application14/core/app_export.dart';
+import 'package:tanisha_s_application14/presentation/disney_hostarconfirmation_successful_transfer_reci_pt_screen/disney_hostarconfirmation_successful_transfer_reci_pt_screen.dart';
 import 'package:tanisha_s_application14/presentation/disney_hostarconfirmation_successful_transfer_screen/disney_hostarconfirmation_successful_transfer_screen.dart';
 import 'package:tanisha_s_application14/widgets/app_bar/appbar_image.dart';
 import 'package:tanisha_s_application14/widgets/app_bar/appbar_image_1.dart';
@@ -8,6 +9,12 @@ import 'package:tanisha_s_application14/widgets/app_bar/appbar_subtitle_2.dart';
 import 'package:tanisha_s_application14/widgets/app_bar/custom_app_bar.dart';
 import 'package:tanisha_s_application14/widgets/custom_bottom_bar.dart';
 import 'package:tanisha_s_application14/widgets/custom_elevated_button.dart';
+import 'package:tanisha_s_application14/widgets/custom_pin_code_text_field.dart';
+import 'package:tanisha_s_application14/widgets/custom_text_form_field.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:provider/provider.dart';
+import '../customer_id_provider.dart';
 
 // ignore_for_file: must_be_immutable
 class PaymentOfDisneyHotstarScreen extends StatelessWidget {
@@ -24,7 +31,8 @@ class PaymentOfDisneyHotstarScreen extends StatelessWidget {
   final FocusNode second = FocusNode();
   final FocusNode third = FocusNode();
   final FocusNode fourth = FocusNode();
-  void onTapVerifyotp(BuildContext context) {
+
+  Future<void> onTapConfirm(BuildContext context, dynamic cid) async {
     if (number_controller.text.isEmpty ||
         number_controller1.text.isEmpty ||
         number_controller2.text.isEmpty ||
@@ -41,13 +49,75 @@ class PaymentOfDisneyHotstarScreen extends StatelessWidget {
     } else {
       // All OTP fields are filled, navigate to the next screen.
       // Navigator.pushNamed(context, AppRoutes.homeLoanPaymentDoneScreen);
+      String dig1 = number_controller.text;
+      String dig2 = number_controller1.text;
+      String dig3 = number_controller2.text;
+      String dig4 = number_controller3.text;
+      String pin = dig1 + dig2 + dig3 + dig4;
 
-      Navigator.push(
-        context,
-        MaterialPageRoute(
-            builder: (context) =>
-                DisneyHostarconfirmationSuccessfulTransferScreen(acc)),
-      );
+      if (cid != null) {
+        var response = await http.post(
+          Uri.parse("http://localhost:5000/payment_interface"),
+          headers: {
+            "Content-Type": "application/json",
+            "token": cid,
+            "pin": pin
+          },
+          body: jsonEncode({"event": "bill_payment", "scheme": "spotify"}),
+        );
+
+        if (response.statusCode == 200) {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) =>
+                    DisneyHostarconfirmationSuccessfulTransferScreen(acc)),
+          );
+        } else if (response.statusCode == 400) {
+          ///Invalid PIN
+          showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                title: Text('Error'),
+                content: Text('Invalid credentials. Please try again.'),
+                actions: <Widget>[
+                  TextButton(
+                    child: Text('OK'),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ],
+              );
+            },
+          );
+        } else if (response.statusCode == 401) {
+          ///Login screen redirect session expire
+          Navigator.pushNamed(context, AppRoutes.loginScreen);
+        } else {
+          ///Some error occured
+          showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                title: Text('Error'),
+                content: Text('Some error occured. Please try again later.'),
+                actions: <Widget>[
+                  TextButton(
+                    child: Text('OK'),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ],
+              );
+            },
+          );
+        }
+      } else {
+        Navigator.pushNamed(context, AppRoutes.loginScreen);
+      }
     }
   }
 
@@ -55,6 +125,12 @@ class PaymentOfDisneyHotstarScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var customerId = Provider.of<CustomerIdProvider>(context).customerId;
+    if (customerId != null)
+      customerId = customerId;
+    else
+      customerId = null;
+
     mediaQueryData = MediaQuery.of(context);
     return SafeArea(
         child: Scaffold(
@@ -301,7 +377,7 @@ class PaymentOfDisneyHotstarScreen extends StatelessWidget {
                             onTap: () {
                               // onTapPaynow(context);
                               // onTapConfirm(context);
-                              onTapVerifyotp(context);
+                              onTapConfirm(context, customerId);
                             },
                           ),
                         )
@@ -326,8 +402,4 @@ class PaymentOfDisneyHotstarScreen extends StatelessWidget {
   /// The [BuildContext] parameter is used to build the navigation stack.
   /// When the action is triggered, this function uses the [Navigator] widget
   /// to push the named route for the disneyHostarconfirmationSuccessfulTransferScreen.
-  onTapConfirm(BuildContext context) {
-    Navigator.pushNamed(
-        context, AppRoutes.disneyHostarconfirmationSuccessfulTransferScreen);
-  }
 }
