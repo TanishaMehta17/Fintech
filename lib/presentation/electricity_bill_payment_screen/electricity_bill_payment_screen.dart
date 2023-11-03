a7e6305d402f281c953f7b29964d2a0c5672e831
nikhilmishra1710 <nikhilmishra1710@gmail.com>
2023-11-04 01:53:53 +0530

    Update electricity_bill_payment_screen.dart

--- lib/presentation/electricity_bill_payment_screen/electricity_bill_payment_screen.dart
+++ lib/presentation/electricity_bill_payment_screen/electricity_bill_payment_screen.dart
@@ -7,7 +7,12 @@ import 'package:tanisha_s_application14/widgets/app_bar/appbar_subtitle_2.dart';
 import 'package:tanisha_s_application14/widgets/app_bar/custom_app_bar.dart';
 import 'package:tanisha_s_application14/widgets/custom_bottom_bar.dart';
 import 'package:tanisha_s_application14/widgets/custom_elevated_button.dart';
+import 'package:tanisha_s_application14/widgets/custom_pin_code_text_field.dart';
 import 'package:tanisha_s_application14/widgets/custom_text_form_field.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:provider/provider.dart';
+import '../customer_id_provider.dart';
 
 // ignore_for_file: must_be_immutable
 class ElectricityBillPaymentScreen extends StatelessWidget {
@@ -27,6 +32,12 @@ class ElectricityBillPaymentScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var customerId = Provider.of<CustomerIdProvider>(context).customerId;
+    if (customerId != null)
+      customerId = customerId;
+    else
+      customerId = null;
+
     mediaQueryData = MediaQuery.of(context);
     return SafeArea(
         child: Scaffold(
@@ -251,7 +262,7 @@ class ElectricityBillPaymentScreen extends StatelessWidget {
                                     width: 350,
                                     onTap: () {
                                       // onTapPaynow(context);
-                                      onTapConfirm(context);
+                                      onTapConfirm(context, customerId);
                                     },
                                   ),
                                 )
@@ -287,8 +298,88 @@ class ElectricityBillPaymentScreen extends StatelessWidget {
   /// The [BuildContext] parameter is used to build the navigation stack.
   /// When the action is triggered, this function uses the [Navigator] widget
   /// to push the named route for the payBillConfigrationElectricityScreen.
-  onTapConfirm(BuildContext context) {
-    Navigator.pushNamed(
-        context, AppRoutes.payBillConfigrationElectricityScreen);
+  Future<void> onTapConfirm(BuildContext context, dynamic cid) async {
+    if (number_controller.text.isEmpty ||
+        number_controller1.text.isEmpty ||
+        number_controller2.text.isEmpty ||
+        number_controller3.text.isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(
+            'All OTP fields must be filled',
+            style: TextStyle(color: Colors.white, fontSize: 16),
+          ),
+          backgroundColor: Colors.red,
+        ),
+      );
+    } else {
+      // All OTP fields are filled, navigate to the next screen.
+      // Navigator.pushNamed(context, AppRoutes.homeLoanPaymentDoneScreen);
+      String dig1 = number_controller.text;
+      String dig2 = number_controller1.text;
+      String dig3 = number_controller2.text;
+      String dig4 = number_controller3.text;
+      String pin = dig1 + dig2 + dig3 + dig4;
+
+      if (cid != null) {
+        var response = await http.post(
+          Uri.parse("http://localhost:5000/payment_interface"),
+          headers: {
+            "Content-Type": "application/json",
+            "token": cid,
+            "pin": pin
+          },
+          body: jsonEncode({"event": "bill_payment", "scheme": "electricity"}),
+        );
+
+        if (response.statusCode == 200) {
+          Navigator.pushNamed(
+              context, AppRoutes.payBillConfigrationElectricityScreen);
+        } else if (response.statusCode == 400) {
+          ///Invalid PIN
+          showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                title: Text('Error'),
+                content: Text('Invalid credentials. Please try again.'),
+                actions: <Widget>[
+                  TextButton(
+                    child: Text('OK'),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ],
+              );
+            },
+          );
+        } else if (response.statusCode == 401) {
+          ///Login screen redirect session expire
+          Navigator.pushNamed(context, AppRoutes.loginScreen);
+        } else {
+          ///Some error occured
+          showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                title: Text('Error'),
+                content: Text('Some error occured. Please try again later.'),
+                actions: <Widget>[
+                  TextButton(
+                    child: Text('OK'),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ],
+              );
+            },
+          );
+        }
+      } else {
+        Navigator.pushNamed(context, AppRoutes.loginScreen);
+      }
+    }
   }
 }
