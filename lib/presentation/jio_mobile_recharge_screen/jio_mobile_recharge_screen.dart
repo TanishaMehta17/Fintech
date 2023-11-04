1eb580f42ee936f2a8c0d6eb6450f5ca6b2b011b
nikhilmishra1710 <nikhilmishra1710@gmail.com>
2023-11-04 09:52:35 +0530

    Update jio_mobile_recharge_screen.dart

--- lib/presentation/jio_mobile_recharge_screen/jio_mobile_recharge_screen.dart
+++ lib/presentation/jio_mobile_recharge_screen/jio_mobile_recharge_screen.dart
@@ -8,6 +8,10 @@ import 'package:tanisha_s_application14/widgets/app_bar/appbar_subtitle_2.dart';
 import 'package:tanisha_s_application14/widgets/app_bar/custom_app_bar.dart';
 import 'package:tanisha_s_application14/widgets/custom_bottom_bar.dart';
 import 'package:tanisha_s_application14/widgets/custom_elevated_button.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:provider/provider.dart';
+import '../customer_id_provider.dart';
 
 // ignore_for_file: must_be_immutable
 class JioMobileRechargeScreen extends StatelessWidget {
@@ -23,7 +27,8 @@ class JioMobileRechargeScreen extends StatelessWidget {
   JioMobileRechargeScreen(this.amt, {Key? key}) : super(key: key);
 
   GlobalKey<NavigatorState> navigatorKey = GlobalKey();
-  void onTapVerifyotp(BuildContext context) {
+
+  Future<void> onTapConfirm(BuildContext context, dynamic cid) async {
     if (number_controller.text.isEmpty ||
         number_controller1.text.isEmpty ||
         number_controller2.text.isEmpty ||
@@ -39,20 +44,87 @@ class JioMobileRechargeScreen extends StatelessWidget {
       );
     } else {
       // All OTP fields are filled, navigate to the next screen.
-      // Navigator.pushNamed(
-      //     context, AppRoutes.jioConfirmationSuccessfulTransferOneScreen);
+      // Navigator.pushNamed(context, AppRoutes.homeLoanPaymentDoneScreen);
+      String dig1 = number_controller.text;
+      String dig2 = number_controller1.text;
+      String dig3 = number_controller2.text;
+      String dig4 = number_controller3.text;
+      String pin = dig1 + dig2 + dig3 + dig4;
 
-      Navigator.push(
-        context,
-        MaterialPageRoute(
-            builder: (context) =>
-                JioConfirmationSuccessfulTransferOneScreen(amt)),
-      );
+      if (cid != null) {
+        var response = await http.post(
+          Uri.parse("http://localhost:5000/payment_interface"),
+          headers: {
+            "Content-Type": "application/json",
+            "token": cid,
+            "pin": pin
+          },
+          body: jsonEncode({"event": "mobile_recharge", "scheme": "jio"}),
+        );
+
+        if (response.statusCode == 200) {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) =>
+                    JioConfirmationSuccessfulTransferOneScreen(amt)),
+          );
+        } else if (response.statusCode == 400) {
+          ///Invalid PIN
+          showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                title: Text('Error'),
+                content: Text('Invalid credentials. Please try again.'),
+                actions: <Widget>[
+                  TextButton(
+                    child: Text('OK'),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ],
+              );
+            },
+          );
+        } else if (response.statusCode == 401) {
+          ///Login screen redirect session expire
+          Navigator.pushNamed(context, AppRoutes.loginScreen);
+        } else {
+          ///Some error occured
+          showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                title: Text('Error'),
+                content: Text('Some error occured. Please try again later.'),
+                actions: <Widget>[
+                  TextButton(
+                    child: Text('OK'),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ],
+              );
+            },
+          );
+        }
+      } else {
+        Navigator.pushNamed(context, AppRoutes.loginScreen);
+      }
     }
   }
 
   @override
   Widget build(BuildContext context) {
+    var customerId = Provider.of<CustomerIdProvider>(context).customerId;
+    if (customerId != null)
+      customerId = customerId;
+    else
+      customerId = null;
+
     mediaQueryData = MediaQuery.of(context);
     return SafeArea(
         child: Scaffold(
@@ -297,7 +369,7 @@ class JioMobileRechargeScreen extends StatelessWidget {
                             width: 350,
                             onTap: () {
                               //onTapPaynow(context);
-                              onTapVerifyotp(context);
+                              onTapConfirm(context, customerId);
                             },
                           ),
                         )
