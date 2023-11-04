4e2fb4c6c8f667701dc20d242c62c5a73e8638ad
nikhilmishra1710 <nikhilmishra1710@gmail.com>
2023-11-04 09:27:37 +0530

    Update marrige_gift_carde_password_screen.dart

--- lib/presentation/marrige_gift_carde_password_screen/marrige_gift_carde_password_screen.dart
+++ lib/presentation/marrige_gift_carde_password_screen/marrige_gift_carde_password_screen.dart
@@ -7,6 +7,10 @@ import 'package:tanisha_s_application14/widgets/app_bar/appbar_subtitle_2.dart';
 import 'package:tanisha_s_application14/widgets/app_bar/custom_app_bar.dart';
 import 'package:tanisha_s_application14/widgets/custom_bottom_bar.dart';
 import 'package:tanisha_s_application14/widgets/custom_elevated_button.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:provider/provider.dart';
+import '../customer_id_provider.dart';
 
 // ignore_for_file: must_be_immutable
 class MarrigeGiftCardePasswordScreen extends StatelessWidget {
@@ -18,7 +22,8 @@ class MarrigeGiftCardePasswordScreen extends StatelessWidget {
   final FocusNode second = FocusNode();
   final FocusNode third = FocusNode();
   final FocusNode fourth = FocusNode();
-  void onTapVerifyotp(BuildContext context) {
+
+  Future<void> onTapConfirm(BuildContext context, dynamic cid) async {
     if (number_controller.text.isEmpty ||
         number_controller1.text.isEmpty ||
         number_controller2.text.isEmpty ||
@@ -34,17 +39,89 @@ class MarrigeGiftCardePasswordScreen extends StatelessWidget {
       );
     } else {
       // All OTP fields are filled, navigate to the next screen.
-      Navigator.pushNamed(
-          context, AppRoutes.marriageGiftConfirmationSuccessfulTransferScreen);
+      // Navigator.pushNamed(context, AppRoutes.homeLoanPaymentDoneScreen);
+      String dig1 = number_controller.text;
+      String dig2 = number_controller1.text;
+      String dig3 = number_controller2.text;
+      String dig4 = number_controller3.text;
+      String pin = dig1 + dig2 + dig3 + dig4;
+
+      if (cid != null) {
+        var response = await http.post(
+          Uri.parse("http://localhost:5000/payment_interface"),
+          headers: {
+            "Content-Type": "application/json",
+            "token": cid,
+            "pin": pin
+          },
+          body: jsonEncode({"event": "gift_card", "scheme": "marriage"}),
+        );
+
+        if (response.statusCode == 200) {
+          Navigator.pushNamed(context,
+              AppRoutes.marriageGiftConfirmationSuccessfulTransferScreen);
+        } else if (response.statusCode == 400) {
+          ///Invalid PIN
+          showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                title: Text('Error'),
+                content: Text('Invalid credentials. Please try again.'),
+                actions: <Widget>[
+                  TextButton(
+                    child: Text('OK'),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ],
+              );
+            },
+          );
+        } else if (response.statusCode == 401) {
+          ///Login screen redirect session expire
+          Navigator.pushNamed(context, AppRoutes.loginScreen);
+        } else {
+          ///Some error occured
+          showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                title: Text('Error'),
+                content: Text('Some error occured. Please try again later.'),
+                actions: <Widget>[
+                  TextButton(
+                    child: Text('OK'),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ],
+              );
+            },
+          );
+        }
+      } else {
+        Navigator.pushNamed(context, AppRoutes.loginScreen);
+      }
     }
   }
 
-  MarrigeGiftCardePasswordScreen({Key? key}) : super(key: key);
+  var name, accNo, amt;
+  MarrigeGiftCardePasswordScreen(this.name, this.accNo, this.amt, {Key? key})
+      : super(key: key);
 
   GlobalKey<NavigatorState> navigatorKey = GlobalKey();
 
   @override
   Widget build(BuildContext context) {
+    var customerId = Provider.of<CustomerIdProvider>(context).customerId;
+    if (customerId != null)
+      customerId = customerId;
+    else
+      customerId = null;
+
     mediaQueryData = MediaQuery.of(context);
     return SafeArea(
         child: Scaffold(
@@ -129,7 +206,7 @@ class MarrigeGiftCardePasswordScreen extends StatelessWidget {
                                                             Padding(
                                                               padding:
                                                                   const EdgeInsets
-                                                                      .only(
+                                                                          .only(
                                                                       left: 0),
                                                               child: Container(
                                                                 child:
@@ -179,7 +256,7 @@ class MarrigeGiftCardePasswordScreen extends StatelessWidget {
                                                             Padding(
                                                               padding:
                                                                   const EdgeInsets
-                                                                      .only(
+                                                                          .only(
                                                                       left: 15),
                                                               child: Container(
                                                                 child:
@@ -228,7 +305,7 @@ class MarrigeGiftCardePasswordScreen extends StatelessWidget {
                                                             Padding(
                                                               padding:
                                                                   const EdgeInsets
-                                                                      .only(
+                                                                          .only(
                                                                       left: 25),
                                                               child: Container(
                                                                 child:
@@ -277,7 +354,7 @@ class MarrigeGiftCardePasswordScreen extends StatelessWidget {
                                                             Padding(
                                                               padding:
                                                                   const EdgeInsets
-                                                                      .only(
+                                                                          .only(
                                                                       left: 25),
                                                               child: Container(
                                                                 child:
@@ -321,15 +398,16 @@ class MarrigeGiftCardePasswordScreen extends StatelessWidget {
                                                         Padding(
                                                           padding:
                                                               const EdgeInsets
-                                                                  .only(
+                                                                      .only(
                                                                   top: 140),
                                                           child:
                                                               CustomElevatedButton(
                                                             text: 'Confirm',
                                                             width: 350,
                                                             onTap: () {
-                                                              onTapVerifyotp(
-                                                                  context);
+                                                              onTapConfirm(
+                                                                  context,
+                                                                  customerId);
                                                             },
                                                           ),
                                                         )
@@ -441,8 +519,4 @@ class MarrigeGiftCardePasswordScreen extends StatelessWidget {
   /// The [BuildContext] parameter is used to build the navigation stack.
   /// When the action is triggered, this function uses the [Navigator] widget
   /// to push the named route for the marriageGiftConfirmationSuccessfulTransferScreen.
-  onTapConfirm(BuildContext context) {
-    Navigator.pushNamed(
-        context, AppRoutes.marriageGiftConfirmationSuccessfulTransferScreen);
-  }
 }
